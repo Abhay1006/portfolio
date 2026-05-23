8f10ecbeca872b7b888ba1f0601ca7afa34d777c
Abhay Pratap Singh <abhaypratapsingh1006@gmail.com>
2026-05-23 22:20:07 +0530

    adding random quotes in the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import InstagramIcon from "@mui/icons-material/Instagram";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import EmailIcon from "@mui/icons-material/Email";
 import BookIcon from "@mui/icons-material/Book";
+import { QUOTES } from "../helpers/QuotesList";
 import "../styles/Footer.css";
 
 function Footer() {
+  const [quote, setQuote] = useState("");
+
+  useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * QUOTES.length);
+    setQuote(QUOTES[randomIndex]);
+  }, []);
+
   return (
     <div className='footer'>
+      {quote && (
+        <div className='quoteContainer'>
+          <p className='quoteText'>“{quote}”</p>
+        </div>
+      )}
       <div className='socialMedia'>
         <Link to="https://www.instagram.com/pratap_abhay.10/" target="_blank" rel="noopener noreferrer">
           <InstagramIcon />
